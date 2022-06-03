df10dfd9b3bc1b86d94a7457405e490cab03cbce
Enes Kara <106449133+by-black6@users.noreply.github.com>
2022-06-04 00:14:56 +0300

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo wget https://github.com/by-black6/ngrok.git
+sudo wget https://github.com/by-black6/ngrokpacket.git
 sudo ./ngrok authtoken 1ySmoWFDth7AaPENnT4uzoyKEQ9_2tnfcof9NZK2QQNkCqeHL
